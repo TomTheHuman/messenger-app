ae2ae8011c64f806e50fb74c0a449c5a7a9345dc
Thomas Shaw <thomas@tomthehuman.com>
2021-06-30 19:29:11 -0700

    Last Message Text Fix Missing from Branch - I implemented a fix for last message text after fixing message order - That fix was missed in last commit and had been overwritten - I have added it back in to preview text from last index instead of 0

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,9 +20,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "ASC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "ASC"] },
+        { model: Message, order: ["createdAt", "DESC"] },
         {
           model: User,
           as: "user1",
@@ -72,7 +72,9 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      // accounts for messages retreived in ascending chronological order
+      let lastMessageIndex = convoJSON.messages.length - 1;
+      convoJSON.latestMessageText = convoJSON.messages[lastMessageIndex].text;
       conversations[i] = convoJSON;
     }
 
